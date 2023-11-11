bbc12f2472c80ce00e0bfb0e596f6e50b34e91e5
hhorika <marislaiman@gmail.com>
2023-11-11 10:28:34 -0500

    finished tasks

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function slugify(title) {
+  const lowerCaseWords = title.toLowerCase().split(" ");
+  const slug = lowerCaseWords.join("-");
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
